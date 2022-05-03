7f9d54354512aed6595ddcdfe6cebda8541daf05
Mat <silencekrakens@gmail.com>
2022-05-04 01:54:00 +0200

    Updated 1.0.1

--- src/main/java/net/acticraft/plugins/beehubcore/BeeHubCore.java
+++ src/main/java/net/acticraft/plugins/beehubcore/BeeHubCore.java
@@ -1,8 +1,12 @@
 package net.acticraft.plugins.beehubcore;
 
+import net.acticraft.plugin.api.beeapi.BeeAPI;
+import net.acticraft.plugin.api.beeapi.mysql.BeeMySQL;
+import net.acticraft.plugins.beehubcore.BlockReplaceListener.BlockReplace;
 import net.acticraft.plugins.beehubcore.events.Events;
 import net.acticraft.plugins.beehubcore.guis.GameMenuGUI;
 import net.acticraft.plugins.beehubcore.playerlisteners.JoinMessageListener;
+import net.acticraft.plugins.beehubcore.scoreboard.ScoreBoard;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BeeHubCore extends JavaPlugin {
@@ -10,11 +14,16 @@ public final class BeeHubCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        instance = this;
+
         getServer().getPluginManager().registerEvents(new Events(), this);
         getServer().getPluginManager().registerEvents(new JoinMessageListener(), this);
         getServer().getPluginManager().registerEvents(new GameMenuGUI(), this);
+        getServer().getPluginManager().registerEvents(new ScoreBoard(), this);
+        getServer().getPluginManager().registerEvents(new BlockReplace(), this);
 
         getCommand("gamemenu").setExecutor(new GameMenuGUI());
+
 
 
     }
@@ -26,5 +35,6 @@ public final class BeeHubCore extends JavaPlugin {
     public static BeeHubCore getInstance() {
         return instance;
     }
+
 }
 
---
+++ src/main/java/net/acticraft/plugins/beehubcore/scoreboard/ScoreBoard.java
@@ -0,0 +1,70 @@
+package net.acticraft.plugins.beehubcore.scoreboard;
+
+import net.acticraft.plugin.api.beeapi.scoreboard.BeeScoreboard;
+import net.acticraft.plugins.beehubcore.BeeHubCore;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Statistic;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class ScoreBoard implements Listener {
+    private final Map<UUID, BeeScoreboard> boards = new HashMap<>();
+
+    public ScoreBoard() {
+        Bukkit.getServer().getScheduler().runTaskTimer(BeeHubCore.getInstance(), () -> {
+            for (BeeScoreboard board : this.boards.values()) {
+                updateBoard(board);
+            }
+        }, 0, 20);
+    }
+
+    @EventHandler
+
+    public void onJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+
+        BeeScoreboard beesc = new BeeScoreboard(player);
+
+        beesc.updateTitle(ChatColor.of("#FFBF00")+""+ChatColor.BOLD + "McBee");
+
+        this.boards.put(player.getUniqueId(), beesc);
+
+
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+
+        BeeScoreboard beesc = this.boards.remove(player.getUniqueId());
+
+        if (beesc != null) {
+            beesc.delete();
+        }
+
+    }
+
+    //TODO do a scoreboard
+    private void updateBoard(BeeScoreboard lobbysb) {
+        lobbysb.updateLines(
+                "",
+                ChatColor.of("#738291")+""+ChatColor.BOLD+"» "+ChatColor.of("#F28C28")+""+ChatColor.BOLD+"Name: " + ChatColor.of("#FAD5A5")+lobbysb.getPlayer().getName(),
+                "",
+                ChatColor.of("#738291")+""+ChatColor.BOLD+"» "+ChatColor.of("#F28C28")+""+ChatColor.BOLD+"Ping: " + ChatColor.of("#FAD5A5")+lobbysb.getPlayer().getPing(),
+                "",
+                ChatColor.of("#FFBF00")+""+ChatColor.BOLD + "discord.gg/mcbee"
+
+
+        );
+
+
+    }
+}
